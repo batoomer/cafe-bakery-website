927493dd5323d8d06e3fd75c88c42ec67adfe614
Batouchan Omer <batouchan.omer@gmail.com>
2024-06-12 16:15:26 +0300

    fixes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import './globalStyle.css';
 import Navigation from './Navigation';
 
-new Navigation()
+new Navigation();
+
+// Add an event listener for when the page has finished loading
+window.addEventListener('beforeunload', () => {
+    // Scroll to the top of the page
+    window.scrollTo(0, 0);
+});
